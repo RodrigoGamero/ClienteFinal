e935932be476934d2f103ba1ac390ebac95bce2a
RodrigoGamero <Rodrigo.gamero.luque@gmail.com>
2021-11-24 12:31:24 +0100

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// VariableS:
+let item, titulo, autor, publicado, enlaceLibro, imagenLibro;
+
+let enlacePeticion = "https://www.googleapis.com/books/v1/volumes?q=";
+let apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+let datos;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+
+});

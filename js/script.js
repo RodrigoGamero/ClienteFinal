d8f04bc2276544e9ff70f17f227f4991a2b439c1
RodrigoGamero <Rodrigo.gamero.luque@gmail.com>
2021-11-25 12:52:47 +0100

    funciona

--- js/script.js
+++ js/script.js
@@ -1,11 +1,60 @@
 // VariableS:
 let item, titulo, autor, publicado, enlaceLibro, imagenLibro;
 
-let enlacePeticion = "https://www.googleapis.com/books/v1/volumes?q=";
-let apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
-let datos;
+let enlacePeticion = "https://www.googleapis.com/books/v1/volumes?q=tittle:";
+let apiKey = "key=AIzaSyBD6LD3app1Bfa-Htlz1aTZulX1H7sRkvA";
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-
 });
+
+// document.getElementById("").addEventListener("click", extraeDatos());
+
+
+
+
+// Funciones:
+function extraeDatos() {
+  let busqueda = document.getElementById("busqueda").value.replace(" ", "+");
+  let url = enlacePeticion + busqueda;
+  console.log(url);
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (result) {
+    let items = result.items;
+    console.log(items);
+    console.log(items.length);
+    for(let i = 0; i < items.length; i++){
+      let y = document.createElement("div");
+      let autor = items[i].volumeInfo.authors[0];
+      let titulo = items[i].volumeInfo.title;
+      let imagen = "";
+      
+      if(items[i].volumeInfo.hasOwnProperty("imageLinks")){
+        imagen = items[i].volumeInfo.imageLinks['thumbnail'];
+      }else{  
+          imagen = "https://www.magisnet.com/wp-content/uploads/2020/05/pagina4libros.jpg";
+      }
+      
+      let x = formato(imagen, titulo, autor);
+      y.innerHTML = x;
+      document.getElementById('libros').appendChild(y);
+    }
+  });
+}
+
+
+
+function formato(imagen, titulo, autor) {
+  // var viewUrl = 'book.html?isbn='+isbn;
+  var htmlCard = `<div class="tarjetaLibro">
+    <div><img src="${imagen}" alt=""></div>
+    <div><p class="titulo">${titulo}</p>
+    <p class="autor">${autor}</p></div>
+    </div>`
+  return htmlCard;
+}
+// });
+
+
+
